a758d5f0e20d7b8d9dbc92f04b0c2a8916957519
gnarwall.org@gmail.com <gnarwall.org@gmail.com@2af07c0d-2c94-18e5-4e81-b1be2220c699>
2010-11-07 05:46:18 +0000

    Added support for disabling the ipv6 network stack in gnarwall_setup.sh

--- trunk/system/usr/local/sbin/gnarwall_reset.sh
+++ trunk/system/usr/local/sbin/gnarwall_reset.sh
@@ -4,7 +4,19 @@
 #
 # Remove configuration files so they will be recreated later.
 
-CONFFILE=/etc/gnarwall/configured
+GW_CONFIGURED=/etc/gnarwall/configured
 
-cd /etc && rm -f resolv.conf timezone hostname mailname hosts ntp.conf
-[ -e $CONFFILE ] && rm -f $CONFFILE
+CONF_FILES='
+/etc/resolv.conf
+/etc/timezone
+/etc/hostname
+/etc/mailname
+/etc/hosts
+/etc/ntp.conf
+/etc/default/locale
+/etc/logcheck/ignore.d.server/rsyslog
+/etc/modprobe.d/00local.conf
+'
+
+for i in `echo $CONF_FILES`; do rm -f $i 2>/dev/null; done
+[ -e $GW_CONFIGURED ] && rm -f $GW_CONFIGURED
