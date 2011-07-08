3d23a0661fc4a1a885c29014fc692dd5899d571e
gnarwall.org@gmail.com <gnarwall.org@gmail.com@2af07c0d-2c94-18e5-4e81-b1be2220c699>
2011-07-08 21:44:38 +0000

    Used bash list in gnarwall_reset.sh for "best practices".

--- system/usr/local/sbin/gnarwall_reset.sh
+++ system/usr/local/sbin/gnarwall_reset.sh
@@ -2,23 +2,21 @@
 
 # gnarwall-reset.sh
 #
-# Remove configuration files so they will be recreated later.
+# Remove configuration files so they will be recreated by
+# gnarwall_setup.sh
 
-# The semaphore file.  Its presense signifies a configured system.
-gnarwall_conf=/etc/gnarwall/configured
+# List of filenames
+conf_files=(
+'/etc/resolv.conf'
+'/etc/timezone'
+'/etc/hostname'
+'/etc/mailname'
+'/etc/hosts'
+'/etc/ntp.conf'
+'/etc/default/locale'
+'/etc/locale.gen'
+'/etc/network/if-pre-up.d/disable_ipv6'
+'/etc/gnarwall/configured'
+)
 
-# List of filenames (no whitespace or wildcards) to remove.
-conf_files='
-/etc/resolv.conf
-/etc/timezone
-/etc/hostname
-/etc/mailname
-/etc/hosts
-/etc/ntp.conf
-/etc/default/locale
-/etc/locale.gen
-/etc/network/if-pre-up.d/disable_ipv6
-'
-
-for i in `echo $conf_files`; do rm -f "$i" 2>/dev/null; done
-[ -e $gnarwall_conf ] && rm -f $gnarwall_conf
+rm -f "${conf_files[@]}" 2>/dev/null
